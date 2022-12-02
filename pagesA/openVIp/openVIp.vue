2567780a88dcf9407c7022d511f47ff6700403d5
117139 <1171390035@qq.com>
2022-12-02 20:55:39 +0800

    Create openVIp.vue

---
+++ pagesA/openVIp/openVIp.vue
@@ -0,0 +1,182 @@
+<template>
+	<view class="wrap_box">
+		<!-- <uParse v-if="datas" :content="datas"></uParse> -->
+		<vip :curtype="cur" backtype="1" :key="'vip'+cur"></vip>
+		
+		<!-- 阻止滑动 -->
+		<!-- <view @touchmove.stop.prevent='test'></view> -->
+	</view>
+</template>
+
+<script>
+	import Vue from 'vue'
+	import vip from '@/components/vip/vip.vue'
+	import {
+		mapState,
+		mapMutations
+	} from 'vuex'
+	var that 
+	export default {
+		components:{vip},
+		data() {
+			return {
+				options:'',
+				datas:'',
+				page:1,
+				cur:0
+			}
+		},
+		computed: {
+		...mapState(['hasLogin', 'forcedLogin', 'userName', 'userinfo','loginDatas']),
+		},
+		// onReachBottom() {
+		// 	that.getdata()
+		// },
+		onLoad(e) {
+			that=this
+			that.options=e||{}
+			console.log(e)
+			that.cur=e.type
+			// that.getdata()
+		},
+		onShow() {
+			// that.onRetry()
+			
+		},
+		
+		methods: {
+			// ...mapMutations(['wxshouquan','login']),
+			test(){},
+			onRetry(){
+				that.page=1
+				that.datas=[]
+				that.getdata()
+			},
+			getdata(){
+				
+				var datas={
+					// day:that.date,
+					page: that.page
+				}
+				uni.showLoading({
+					mask:true,
+					title:'正在获取数据'
+				})
+				var jkurl='/history'
+				var nowpage=that.page
+				that.$service.P_post(jkurl, datas).then(res => {
+					that.btnkg = 0
+					console.log(res)
+					if (res.code == 1) {
+						that.htmlReset = 0
+						var datas = res.data
+						console.log(typeof datas)
+				
+						if (typeof datas == 'string') {
+							datas = JSON.parse(datas)
+						}
+						console.log(res)
+						if(nowpage==1){
+							that.datas=datas.data
+						}else{
+							if(datas.data.length==0){
+								return
+							}
+							that.datas=that.datas.concat(datas.data)
+						}
+						if(datas.data.length==0){
+							return
+						}
+						that.page++
+					} else {
+					
+						if (res.msg) {
+							uni.showToast({
+								icon: 'none',
+								title: res.msg
+							})
+						} else {
+							uni.showToast({
+								icon: 'none',
+								title: '获取数据失败'
+							})
+						}
+					}
+				}).catch(e => {
+					that.htmlReset = 1
+					that.btnkg = 0
+					// that.$refs.htmlLoading.htmlReset_fuc(1)
+					console.log(e)
+					uni.showToast({
+						icon: 'none',
+						title: '获取数据失败，请检查您的网络连接'
+					})
+				})
+			},
+			// 单条数据
+			getdata1(){
+				
+				var datas={
+					id: that.options.id
+				}
+				var jkurl='/news_detail'
+				
+				that.$service.P_post(jkurl, datas).then(res => {
+					that.btnkg = 0
+					console.log(res)
+					if (res.code == 1) {
+						that.htmlReset = 0
+						var datas = res.data
+						console.log(typeof datas)
+				
+						if (typeof datas == 'string') {
+							datas = JSON.parse(datas)
+						}
+						console.log(res)
+						that.datas=datas.content
+						// if(datas.title){
+						// 	uni.setNavigationBarTitle({
+						// 		title:datas.title
+						// 	})
+						// }
+					} else {
+					
+						if (res.msg) {
+							uni.showToast({
+								icon: 'none',
+								title: res.msg
+							})
+						} else {
+							uni.showToast({
+								icon: 'none',
+								title: '获取数据失败'
+							})
+						}
+					}
+				}).catch(e => {
+					that.htmlReset = 1
+					that.btnkg = 0
+					// that.$refs.htmlLoading.htmlReset_fuc(1)
+					console.log(e)
+					uni.showToast({
+						icon: 'none',
+						title: '获取数据失败，请检查您的网络连接'
+					})
+				})
+			},
+			
+			goback(){
+			  uni.navigateBack()
+			},
+			func(){
+				// that.$store.commit('setSystem',datas.system)
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+page{
+	// background: #fff;
+}
+</style>
